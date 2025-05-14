fcd177610cf022c6b05c8565c5b3954e795c7bf6
NoyeArk <qilei.zhou6@gmail.com>
2025-05-14 10:05:33 +0800

    finish chapter2

--- read_data.py
+++
@@ -1,5 +0,0 @@
-# Listing 2.1 Reading in a short story as text sample into Python
-with open("the-verdict.txt", "r", encoding="utf-8") as f:
-		raw_text = f.read()
-print("Total number of character:", len(raw_text))
-print(raw_text[:99])
